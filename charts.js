9f65614e8268a8d99f9de7b9a8cc8c21b7654f5b
megan o <mooi0004@student.monash.edu>
2020-10-11 23:09:22 +0800

    first push

---
+++ charts.js
@@ -0,0 +1,4 @@
+var spec = "https://alxojy.github.io/econs-viz/json_charts/bubble_plot.json";
+  vegaEmbed('#bubble', spec).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+  }).catch(console.error);
