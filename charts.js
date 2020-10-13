eb3fe256108d5e91971eaf44d8907c590112b5c9
alex o <40974140+alxojy@users.noreply.github.com>
2020-10-13 21:29:09 +0800

    Update charts.js

--- charts.js
+++ charts.js
@@ -2,3 +2,8 @@ var spec = "https://alxojy.github.io/econs-viz/json_charts/bubble_plot.json";
   vegaEmbed('#bubble', spec).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
   }).catch(console.error);
+
+var spec = "https://alxojy.github.io/econs-viz/json_charts/map.json";
+  vegaEmbed('#gdp-map', spec).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+  }).catch(console.error);
