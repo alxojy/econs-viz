1b6d484ae7ae13f84b31ab814490f4fb1b1f8b65
megan o <mooi0004@student.monash.edu>
2020-10-14 23:35:56 +0800

    correct values

--- charts.js
+++ charts.js
@@ -10,5 +10,8 @@ var spec = "https://alxojy.github.io/econs-viz/json_charts/map.json";
 
 var spec = "https://alxojy.github.io/econs-viz/json_charts/waterfall.json";
 vegaEmbed('#waterfall', spec).then(function(result) {
+  result.transform.filter("datum.country == 'Japan'");
+  //result.spec.layer[0].transform.filter = "datum.country == 'Japan'";
+  console.log(result.spec.layer[0]);
   // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
--- data/clean.py
+++ data/clean.py
@@ -3,20 +3,19 @@ out = open('life_satisfaction.csv', 'w')
 prev = ''
 for i, line in enumerate(life):
     test  = line.split(',')
-    if test[2].strip() == '2018' or test[2].strip() == '2017' or test[2].strip() == '2007' or test[2].strip() == '2006':
-        pass
-    else:
+    if 2005 < int(test[2].strip()) < 2017:
+
         if prev == '' or prev[0] != test[0]:
+            print(prev, test)
             start = 0
         else:
             start = prev[4]
         end = test[1]
 
-        if i == 0:
-            pass
-        else:
-            cat = "increase" if (float(end)-float(start) > 0) else "decrease"
-            out.write(str(test[0]) + ',' + str(test[2].strip()) + ',' + str(start) + ',' + str(end) + ',' + cat + '\n')
+        cat = "increase" if (float(end)-float(start) > 0) else "decrease"
+        out.write(str(test[0]) + ',' + str(test[2].strip()) + ',' + str(start) + ',' + str(end) + ',' + cat + '\n')
         
+        prev = test + [start, end]
 
-    prev = test + [start, end]
+life.close()
+out.close()
