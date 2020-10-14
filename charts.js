53e37bba99a096a6eaeec5ae2bec0c3225adaa33
megan o <mooi0004@student.monash.edu>
2020-10-14 15:38:57 +0800

    done waterfall chart

--- charts.js
+++ charts.js
@@ -8,11 +8,7 @@ var spec = "https://alxojy.github.io/econs-viz/json_charts/map.json";
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
   }).catch(console.error);
 
-var slider = document.getElementById("myRange");
-var output = document.getElementById("demo");
-output.innerHTML = slider.value; // Display the default slider value
-
-// Update the current slider value (each time you drag the slider handle)
-slider.oninput = function() {
-  output.innerHTML = this.value;
-}
+var spec = "https://alxojy.github.io/econs-viz/json_charts/waterfall.json";
+vegaEmbed('#waterfall', spec).then(function(result) {
+  // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
