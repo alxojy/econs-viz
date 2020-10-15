9060512c96e8952ab48c4a4d98cd418ac84f1217
megan o <mooi0004@student.monash.edu>
2020-10-15 13:37:21 +0800

    2 interactive plots

--- charts.js
+++ charts.js
@@ -3,15 +3,7 @@ var spec = "https://alxojy.github.io/econs-viz/json_charts/bubble_plot.json";
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
   }).catch(console.error);
 
-var spec = "https://alxojy.github.io/econs-viz/json_charts/map.json";
-  vegaEmbed('#gdp-map', spec).then(function(result) {
+var spec = "https://alxojy.github.io/econs-viz/json_charts/map_waterfall.json";
+  vegaEmbed('#map-waterfall', spec).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
   }).catch(console.error);
-
-var spec = "https://alxojy.github.io/econs-viz/json_charts/waterfall.json";
-vegaEmbed('#waterfall', spec).then(function(result) {
-  result.transform.filter("datum.country == 'Japan'");
-  //result.spec.layer[0].transform.filter = "datum.country == 'Japan'";
-  console.log(result.spec.layer[0]);
-  // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
-}).catch(console.error);
