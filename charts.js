4f6b421431b196cf0f75c1e8e9fb29d87acdee09
alex o <40974140+alxojy@users.noreply.github.com>
2020-10-16 12:39:38 +0800

    Update charts.js

--- charts.js
+++ charts.js
@@ -3,7 +3,7 @@ var spec = "https://alxojy.github.io/econs-viz/json_charts/bubble_plot.json";
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
   }).catch(console.error);
 
-var spec = "https://alxojy.github.io/econs-viz/json_charts/map_waterfall_area.json";
+var spec = "https://alxojy.github.io/econs-viz/json_charts/map_waterfall.json";
   vegaEmbed('#map-waterfall-area', spec).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
   }).catch(console.error);
