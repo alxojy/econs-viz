8a860740ef972a847f52215502d3054269f1eb6e
megan o <mooi0004@student.monash.edu>
2020-10-18 15:11:25 +0800

    style css

--- charts.js
+++ charts.js
@@ -7,3 +7,11 @@ var spec = "https://alxojy.github.io/econs-viz/json_charts/map_waterfall.json";
   vegaEmbed('#map-waterfall-area', spec).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
   }).catch(console.error);
+
+var i = 10;
+var inv = setInterval(function() {     
+    if(i < 100)
+        document.getElementById("counter").innerHTML = '$ ' + ++i + ' trillion USD';
+    else
+        clearInterval(inv);
+}, 1000 / 100);
