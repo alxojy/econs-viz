37591fc1110f44158916e3387cfdd0ae30233935
megan o <mooi0004@student.monash.edu>
2020-10-18 23:39:52 +0800

    unit chart

--- charts.js
+++ charts.js
@@ -8,6 +8,11 @@ var spec = "https://alxojy.github.io/econs-viz/json_charts/map_waterfall.json";
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
   }).catch(console.error);
 
+var spec = "https://alxojy.github.io/econs-viz/json_charts/unit_chart.json";
+  vegaEmbed('#unit-chart', spec).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+  }).catch(console.error);
+
 var i = 10;
 var inv = setInterval(function() {     
     if(i < 100)
