cb6750204b3754a0eefa1dba4694c260f39ef594
megan o <mooi0004@student.monash.edu>
2020-10-23 14:39:21 +0800

    remove word cloud

--- charts.js
+++ charts.js
@@ -8,12 +8,6 @@ var spec = "https://alxojy.github.io/econs-viz/json_charts/map_waterfall.json";
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
   }).catch(console.error);
 
-  var spec = "https://alxojy.github.io/econs-viz/json_charts/word_cloud.json";
-  vegaEmbed('#word-cloud', spec).then(function(result) {
-    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
-  }).catch(console.error);
-
-
 var spec = "https://alxojy.github.io/econs-viz/json_charts/unit_chart.json";
   vegaEmbed('#unit-chart', spec).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
